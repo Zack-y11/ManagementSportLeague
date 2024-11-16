3f51f030a16486fc802ac35cdda1617c666888d4
ZackDev <devzacknmr28@gmail.com>
2024-11-16 13:38:38 -0600

    feat: adding some nugets packages and depency injection in program.cs

--- PresentationLayer/Program.cs
+++ PresentationLayer/Program.cs
@@ -1,3 +1,11 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using QuestPDF.Infrastructure;
+using PresentationLayer.Forms;
+using BusinessLayer.Services;
+
+
 namespace PresentationLayer
 {
     internal static class Program
@@ -8,10 +16,50 @@ namespace PresentationLayer
         [STAThread]
         static void Main()
         {
+            QuestPDF.Settings.License = LicenseType.Community;
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Forms.dashboardAdmin());
+            var host = CreateHostBuilder().Build();
+            ServiceProvider = host.Services;
+
+            Application.Run(ServiceProvider.GetRequiredService<dashboardAdmin>());
+        }
+
+        public static IServiceProvider ServiceProvider { get; private set; }
+
+        static IHostBuilder CreateHostBuilder()
+        {
+            return Host.CreateDefaultBuilder()
+                .ConfigureAppConfiguration((context, config) => {
+                    //config.AddJsonFile(
+                    //    "appsettings.json",
+                    //    optional: false,
+                    //    reloadOnChange: true
+                    // );
+                })
+                .ConfigureServices((context, services) => {
+
+                    //Forms
+                    services.AddTransient<dashboardAdmin>();
+
+                    //Repositories
+                    services.AddScoped<IMatchService, CategoryRepository>();
+                    //services.AddScoped<IEmailQueueRepository, EmailQueueRepository>();
+
+                    //Services
+                    //services.AddScoped<ICategoryService, CategoryService>();
+                    //services.AddScoped<IEmailService, EmailService>();
+
+                    //Notifications
+                    //services.AddScoped<IEmailNotification, EmailNotification>();
+
+                    //Reports
+                    //services.AddScoped<ICategoryReport, CategoryReport>();
+
+                    //Connection
+                    //services.AddSingleton<ISqlDataAccess, SqlDataAccess>();
+                });
         }
     }
 }

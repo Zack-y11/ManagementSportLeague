a3db8b31c280b46e4305f2576f59f33ea9a59b50
androso <androso421@gmail.com>
2024-11-16 11:37:22 -0600

    adding connection to db

---
+++ DataLayer/DbConnection/SqlDataAccess.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DataLayer.DbConnection
+{
+    public class SqlDataAccess : ISqlDataAccess
+    {
+        private readonly IConfiguration _configuration;
+        private readonly string _connectionString;
+
+        public SqlDataAccess(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _connectionString = _configuration.GetConnectionString("default");
+        }
+
+        public IDbConnection GetConnection() => new SqlConnection(_connectionString);
+    }
+}

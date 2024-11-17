feca69b02a48628d49582c0e0c2d7ad134123e20
anibal andrade <59257555+androso@users.noreply.github.com>
2024-11-17 11:49:55 -0600

    showing admin dashboard if user == admin (#5)

--- PresentationLayer/Forms/LoginF/LoginForms.cs
+++ PresentationLayer/Forms/LoginF/LoginForms.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using BusinessLayer.Services;
+using CommonLayer.Enums;
+using CommonLayer.Models;
+using Microsoft.Extensions.DependencyInjection;
+using PresentationLayer.Forms;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +17,42 @@ namespace PresentationLayer.LoginF
 {
     public partial class LoginForms : Form
     {
-        public LoginForms()
+        private readonly IUserService _userService;
+        private readonly IServiceProvider _serviceProvider;
+
+        public LoginForms(IUserService userService, IServiceProvider serviceProvider)
         {
             InitializeComponent();
+            _userService = userService;
+            _serviceProvider = serviceProvider;
+        }
+
+        private void SignInButtom_Click(object sender, EventArgs e)
+        {
+            var user = _userService.Login(emailTextBox.Text, passwordTextBox.Text);
+            if (user != null)
+            {
+                AuthenticatedUser.Name = user.Name;
+                AuthenticatedUser.RoleId = user.RoleId;
+
+                if (AuthenticatedUser.RoleId == (int)Roles.Admin)
+                {
+                    _serviceProvider.GetRequiredService<dashboardAdmin>().ShowDialog();
+                    this.Hide();
+                } else if (AuthenticatedUser.RoleId == (int)Roles.Manager)
+                {
+                    // manager o coach
+
+                } else if (AuthenticatedUser.RoleId == (int)Roles.Player)
+                {
+                    // player
+
+                }
+            }else
+            {
+                
+                MessageBox.Show("Invalid email or password", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }

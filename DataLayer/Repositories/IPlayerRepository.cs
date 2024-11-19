06914bc23906f62bc33473b0b7999b73443cef0c
ZackDev <devzacknmr28@gmail.com>
2024-11-19 11:22:26 -0600

    adding crud for players, implementing scoped in program.cs

---
+++ DataLayer/Repositories/IPlayerRepository.cs
@@ -0,0 +1,12 @@
+﻿using CommonLayer.Models;
+
+namespace DataLayer.Repositories
+{
+    public interface IPlayerRepository
+    {
+        void Add(Player player);
+        void Delete(int playerId);
+        IEnumerable<Player> GetAll();
+        void Update(Player player);
+    }
+}
